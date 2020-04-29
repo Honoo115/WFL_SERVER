c6bddfccedb1a62a1c5941091b754d06b09acf7f
unknown <ixentrik@gmail.com>
2020-04-29 11:00:18 -0700

    votes troubleshooting

--- src/voteService/voteService.js
+++ src/voteService/voteService.js
@@ -1,7 +1,6 @@
 const voteService = {
     getVotes(knex, uuid) {
         return knex.select("*").from("votes")
-        .join("restaurants","votes.restaurant_id", "restaurants.id")
         .where("votes.poll_id", uuid)
     },
     addVote(db, voteToAdd) {
