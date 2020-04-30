d29072526a7721d51bcacd3bd38ad0b789f394d5
unknown <ixentrik@gmail.com>
2020-04-30 09:26:00 -0700

    IP Troubleshooting

--- src/pollRouter/pollRouter.js
+++ src/pollRouter/pollRouter.js
@@ -19,7 +19,7 @@ pollRouter
             .catch(next)
     })
     .post(jsonParser, (req, res, next) => {
-        console.log(req.ip)
+        console.log("IP ADDRESS HERE ----->",req.ip)
         let restaurants = [];
 
         let { city, postal_code, uuid } = req.body; // Grab the uuid
